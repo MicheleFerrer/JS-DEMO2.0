1ac2e52e02596496802567291a9e988ac8de721c
MicheleFerrer <michele.e.ferrer@gmail.com>
2024-04-22 21:01:55 -0400

    More JavaScript Progress
    Worked more with different variable, constants, primitive types, declarations, reserved keywords etc.

---
+++ index.js
@@ -0,0 +1,26 @@
+// First JavaScript Code //
+
+
+
+
+// now will be utilizing camel notation and multiple variables/declarations
+
+let firstName = 'michele'; //string literal
+let lastName = 'ferrer';
+console.log(firstName);
+console.log(lastName);
+
+// interest rates //
+
+let interestRate = 0.3;
+interestRate = 1;
+console.log(interestRate);
+
+
+let age = 30; // number literal
+let isApproved = true; //boolean literal can be true or false
+let firstName = undefined;
+let lastName = null; //used to clear the value of a variable
+let selectedColor = null; // when the user selectes the color then youd change it to 'color'
+
+// typeof selected color = "object"
