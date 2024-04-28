1f6f2f41cef1901c221e26a2a2b4e58b281801ac
MicheleFerrer <michele.e.ferrer@gmail.com>
2024-04-28 08:28:30 -0400

    private properties and closure in JavaScript

--- index2.js
+++ index2.js
@@ -31,5 +31,33 @@ console.log(obj);
 
 //user.token ='asdasdsd'; we can add extract properties on the fly
 
-if ('radius' in circle)
-console.log('circle has a radius');
+//if ('radius' in circle)
+//console.log('circle has a radius');
+
+//hide the details and only show the essentials-Private properties and methods-Abstraction
+
+//this. references a new object
+
+function Circle(radius) {
+//let color = 'red' // wont be a property of red because this.color='red' hasnt been set. this is only a local variable inside of this particular function
+this.radius = radius;
+    let defaultLocation = { x: 0, y: 0 }; // implementation detail that we dont want to be accessed from the outside. instead of this.(propertry) we use let to  define it as a local variable
+//let computeOptimumLocation = function(factor){ //allows access to private info
+    this.getDefaultLocation = function(){
+    return defaultLocation;
+    };
+
+
+    this.draw = function() {
+    // this.computeOptimumLocation(0.1);  //this would be used to access private propeties
+    // we can remove the this. due to closure. Closure determines what variable will be accessible to an inner function
+    console.log('draw');
+    };
+        Object.defineProperty(this, 'defaultLocation', {
+           get: function() {
+            return defaultLocation; 
+        }
+        });
+}
+const Circle = new Circle(10);
+circle.draw();
